b59aaa274b67faef75c95e977afd4ce811394930
ChengTsungPao <46835017+ChengTsungPao@users.noreply.github.com>
2021-06-26 00:35:23 +0800

    add_vel

--- client/src/Start/function/KeyBoard.js
+++ client/src/Start/function/KeyBoard.js
@@ -48,7 +48,7 @@ export const MoveEngine = () => {
 
         var move = [0, 0];
         for (let action of keyboard.active.values()){
-            move = [move[0] + MoveEvent(action, user.myUser.vel)[0], move[1] + MoveEvent(action, user.myUser.vel)[1]]
+            move = [move[0] + MoveEvent(action, user.myUser.vel[0])[0], move[1] + MoveEvent(action, user.myUser.vel[1])[1]]
         }
     
         var collision = Collision(user.myUser.r, move, user.allUser, wall, 12, 20, 100, { left: [0, 500], top: [0, 500] });
@@ -60,7 +60,7 @@ export const MoveEngine = () => {
         if (collision.event) {
             var originUser = user.myUser.r
             originUser = [originUser[0] + move[0], originUser[1] + move[1]];
-            SendData("setUser", { r: originUser, vel: VelFunction(user.myUser.vel), kind: "z", name: GetData("name"), room: GetData("room") });
+            SendData("setUser", { r: originUser, vel: [VelFunction(user.myUser.vel[0]), VelFunction(user.myUser.vel[1])], kind: "z", name: GetData("name"), room: GetData("room") });
         }
 
         // console.log(user.myUser.r);
@@ -74,15 +74,21 @@ export const KeyUp = (key) => {
 
     keyboard.active.delete(key);
 
-    if (keyboard.active.size === 0) {
-        SendData("setUser", { r: GetMyUser(SplitData(GetData("updateGameData")).user).r, vel: 1, kind: "z", name: GetData("name"), room: GetData("room") });
+    if (!keyboard.active.has("ArrowUp") && !keyboard.active.has("ArrowDown")){
+        const data = GetMyUser(SplitData(GetData("updateGameData")).user);
+        SendData("setUser", { r: data.r, vel: [data.vel[0], 1], kind: "z", name: GetData("name"), room: GetData("room") });
+    }
+
+    if (!keyboard.active.has("ArrowLeft") && !keyboard.active.has("ArrowRight")){
+        const data = GetMyUser(SplitData(GetData("updateGameData")).user);
+        SendData("setUser", { r: data.r, vel: [1, data.vel[1]], kind: "z", name: GetData("name"), room: GetData("room") });
     }
 
     if (!keyboard.keyup.has(key)) {
         return
     }
 
-    SendData("setUser", { r: [Math.floor(Math.random() * 500), Math.floor(Math.random() * 500)], vel: 1, kind: key, name: GetData("name"), room: GetData("room") });
+    SendData("setUser", { r: [Math.floor(Math.random() * 500), Math.floor(Math.random() * 500)], vel: [1, 1], kind: key, name: GetData("name"), room: GetData("room") });
 
 }
 
